638bf0e4ac4cc8e244698f59a909ab0ec75a7ce7
煩悩少年 <bonnou.shounen@gmail.com>
2020-10-18 23:31:12 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# us-cityheaven
+
+userscript for [cityheaven](https://www.cityheaven.net/tt/community/ABMypageHome)
+
+## install
+
+- install [TamperMonkey](https://www.tampermonkey.net/) into your browser
+- open [raw source](https://raw.githubusercontent.com/bonnou-shounen/us-cityheaven/main/cityheaven-plus.user.js)
---
+++ cityheaven-plus.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name        cityheaven-plus
+// @version     v0.0.1
+// @match       https://www.cityheaven.net/*
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // オフィシャルフラグを削除する
+    if (location.search.match(/\bof=y\b/)) {
+        location.search = location.search.replace(/\bof=y\b/, '')
+        return
+    }
+
+    // 「マイヘブン」を「マイガール/出勤情報」へのリンクにする
+    const myh = document.getElementsByClassName('myh')[0]
+    if (!myh) {
+        return
+    }
+    myh.dataset.forwardHref = myh.dataset.forwardHref.replace('ABMypageHome', 'ABMyAlbumShukkin')
+})()
