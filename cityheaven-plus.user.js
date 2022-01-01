6430fe565c79c89fc097a1f79a11acf8e76db503
煩悩少年 <bonnou.shounen@gmail.com>
2022-01-01 10:49:09 +0900

    shortcuts after "my girl"

--- cityheaven-plus.user.js
+++ cityheaven-plus.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        cityheaven-plus
-// @version     0.0.3
+// @version     0.0.4
 // @match       https://www.cityheaven.net/*
 // ==/UserScript==
 
@@ -15,16 +15,30 @@
 
     // 便利リンクを作る
     const myh = document.getElementsByClassName('myh')[0]
-    if (!myh) {
-        return
-    }
-    const ul = myh.parentNode.parentNode
-    const li = document.createElement('li')
-    ul.insertBefore(li, null)
-    li.innerHTML = `
-        <a href="/mypage/comeonlist/?spmode=pc">キテネ</a>
+    if (myh) {
+      const ul = myh.parentNode.parentNode
+      const li = document.createElement('li')
+      ul.insertBefore(li, null)
+      li.innerHTML = `
         <a href="/tt/community/ABFavoriteGirlList/?spmode=pc">マイガ</a>
         <a href="/tt/community/ABMyAlbumShukkin/?spmode=pc">出勤</a>
-        <a href="/tt/community/SBMyAlbumShukkin/?pcmode=sp">週間</a>
-    `
+        <a href="/tt/community/SBMyAlbumShukkin/?pcmode=sp" target="_blank">週間</a>
+        <a href="/mypage/comeonlist/?spmode=pc" target="_blank">キテネ</a>
+      `
+      return
+    }
+
+    const myg = document.getElementsByClassName('header-wrap-menu-item')[1]
+    if (myg) {
+        const menuDiv = myg.parentNode
+        const div = document.createElement('div')
+        div.className = 'header-wrap-menu-item'
+        menuDiv.insertBefore(div, myg.nextSibling)
+        div.innerHTML = `
+          <a href="/tt/community/ABMyAlbumShukkin/?spmode=pc">出勤</a>
+          ・<a href="/tt/community/SBMyAlbumShukkin/?pcmode=sp" target="_blank">週間</a>
+          ・ <a href="/mypage/comeonlist/?spmode=pc" target="_blank">キテネ</a>
+        `
+        return
+    }
 })()
